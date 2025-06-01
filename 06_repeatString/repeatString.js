d59dc854ccd4d68077288f40093b61b0d39febc1
Tasmia Chowdhury <tasmiachowdhury123@gmail.com>
2025-06-01 01:40:08 -0400

    finished exercise 1-7

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let new_word = ""
+    for(let i =0; i<times; i++){
+        new_word+=word;
+    }
+    return new_word;
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let new_word ="";
+    let arr = word.split("");
+    let reverseArr = arr.reverse()
+    for(let i=0; i<reverseArr.length; i++){
+        new_word+=reverseArr[i]
+    }
+    return new_word
 };
 
 // Do not edit below this line
